bde956905b65c8743b7a51ef7a080515e20d56d5
Frederico Bedin <iguidifred@gmail.com>
2023-04-07 18:29:09 +0100

    changed the method .get and .post to match the front end pattern

--- index.js
+++ index.js
@@ -1,83 +1,87 @@
-const express = require("express")
-const uuid = require("uuid")
-const port = 3000
+const express = require("express");
+const uuid = require("uuid");
+const cors = require("cors");
+const port = 3001;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const orders = []
+const orders = [];
 
+const checkOrderId = (request, response, next) => {
+  const { id } = request.params;
+  const index = orders.findIndex((order) => order.id === id);
 
-const checkOrderId = (request, response, next) =>{
-    const { id } = request.params
-    const index = orders.findIndex(order => order.id === id)
-    
-    if( index < 0 ){
-        return response.status(404).json({message: "order not found"})
-    }
+  if (index < 0) {
+    return response.status(404).json({ message: "order not found" });
+  }
 
-    request.userIndex = index
-    request.userId = id
+  request.userIndex = index;
+  request.userId = id;
 
-    next()
-}
+  next();
+};
 
 const checkMethodURL = (request, response, next) => {
-    console.log(request.method, request.url)
-    next()
-}
- 
+  console.log(request.method, request.url);
+  next();
+};
 
 app.post(`/order`, checkMethodURL, (request, response) => {
-    const {itens, clientName, price} = request.body
-    const order = {id:uuid.v4(), itens, clientName, price, status:"Preparing your Order"}
+  const { itens, clientName, price } = request.body;
+  const order = {
+    id: uuid.v4(),
+    itens,
+    clientName,
+    price,
+    status: "Preparing your Order",
+  };
 
-    orders.push(order)
-    return response.status(201).json(order)
-})
+  orders.push(order);
+  return response.status(201).json(order);
+});
 app.get(`/order`, checkMethodURL, (request, response) => {
-    return response.json(orders)
-})
+  return response.json(orders);
+});
 app.put(`/order/:id`, checkMethodURL, checkOrderId, (request, response) => {
-    const {itens, clientName, price} = request.body
-    const index = request.userIndex
-    const id = request.userId
+  const { itens, clientName, price } = request.body;
+  const index = request.userIndex;
+  const id = request.userId;
 
-    const updateOrder = {id, itens, clientName, price}
+  const updateOrder = { id, itens, clientName, price };
 
-    orders[index] = updateOrder
+  orders[index] = updateOrder;
 
-    return response.json(updateOrder)
-})
+  return response.json(updateOrder);
+});
 app.delete(`/order/:id`, checkMethodURL, checkOrderId, (request, response) => {
-    const index = request.orderIndex
-    orders.splice(index,1)
+  const index = request.orderIndex;
+  orders.splice(index, 1);
 
-    return response.status(204).json()
-})
+  return response.status(204).json();
+});
 app.get(`/order/:id`, checkMethodURL, checkOrderId, (request, response) => {
-    const id = request.params.id
-    const order = orders.find(order => order.id === id)
-    if (order) {
-    response.send(order)
-    } else {
-    response.status(404).send('Order not found')
-    }
-})
+  const id = request.params.id;
+  const order = orders.find((order) => order.id === id);
+  if (order) {
+    response.send(order);
+  } else {
+    response.status(404).send("Order not found");
+  }
+});
 app.patch(`/order/:id`, checkMethodURL, checkOrderId, (request, response) => {
-    const { id } = request.params
-    const { status } = request.body
-    const order = orders.find(order => order.id === id)
-    if (!order) {
-    return response.status(404).send('Order not found')
-    }
+  const { id } = request.params;
+  const { status } = request.body;
+  const order = orders.find((order) => order.id === id);
+  if (!order) {
+    return response.status(404).send("Order not found");
+  }
 
-    order.status = status
-    return response.send(order)
-})
-
-
+  order.status = status;
+  return response.send(order);
+});
 
 app.listen(port, () => {
-    console.log(`🚀Code is running on the port ${port}`)
-})
+  console.log(`🚀Code is running on the port ${port}`);
+});
